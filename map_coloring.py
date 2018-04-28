61ebf93043db4694cfcee4ea0c3140e375c965b1
Mertcan Gürcan <mertcan@kodgemisi.com>
2018-04-28 12:03:41 +0300

    Fix variable name

--- map_coloring.py
+++ map_coloring.py
@@ -5,8 +5,11 @@ import numpy as np
 def initiate_population():
 	num_of_regions = 4;
 	adjacency_matrix = np.random.randint(0,2,(num_of_regions,num_of_regions))
+	all_adjacencies = []
 	for i in range (0,num_of_regions):
 		adjacency_matrix = np.random.randint(0,2,(num_of_regions,num_of_regions))
 		np.fill_diagonal(adjacency_matrix,0)
-		all_adjecencies.append(adjacency_matrix.tolist())
+		all_adjacencies.append(adjacency_matrix.tolist())
 	regions = np.random.randint(0, num_of_regions + 1 , num_of_regions)
+	
+
