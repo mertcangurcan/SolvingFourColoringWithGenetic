454bd5f453f8e098b2616ec9c027651dd715223d
Mertcan Gürcan <mertcan@kodgemisi.com>
2018-04-28 13:14:24 +0300

    add list region set

--- map_coloring.py
+++ map_coloring.py
@@ -1,15 +1,26 @@
 import random
 import numpy as np
+from IPython import embed
+
 
 # initilazation of first population randomly
 def initiate_population():
 	num_of_regions = 4;
-	adjacency_matrix = np.random.randint(0,2,(num_of_regions,num_of_regions))
 	all_adjacencies = []
+	region_set = []
 	for i in range (0,num_of_regions):
 		adjacency_matrix = np.random.randint(0,2,(num_of_regions,num_of_regions))
 		np.fill_diagonal(adjacency_matrix,0)
 		all_adjacencies.append(adjacency_matrix.tolist())
-	regions = np.random.randint(0, num_of_regions + 1 , num_of_regions)
-	
+	for i in range (0, 20):
+		region_set.append(np.random.randint(0, num_of_regions + 1 , num_of_regions))
+	return region_set, all_adjacencies
 
+def print_zones(region_set):
+	for i in range (0, len(region_set)):
+		for j in range (0, len((region_set[0]))):
+			print("{0}.set Zone {1} color_code : {2}".format(i, j, region_set[i][j]))
+
+region_set, neighborhood = initiate_population()
+print_zones(region_set)
+	
